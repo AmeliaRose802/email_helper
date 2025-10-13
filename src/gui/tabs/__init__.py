40ef8660ad1a8289dd53461d2cdea058bb92dfc6
Amelia Payne <ameliapayne@microsoft.com>
2025-10-13 02:42:41 -0700

    🎉 DELETE THE NIGHTMARE - Remove 3703-line monolith and complete MVC refactoring

--- src/gui/tabs/__init__.py
+++ src/gui/tabs/__init__.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """GUI Tabs Package - View components for email helper application."""
 
-from gui.tabs.processing_tab_refactored import ProcessingTab
-from gui.tabs.editing_tab_refactored import EditingTab
+from gui.tabs.processing_tab import ProcessingTab
+from gui.tabs.editing_tab import EditingTab
 from gui.tabs.summary_tab import SummaryTab
 from gui.tabs.accuracy_tab import AccuracyTab
 
--- src/viewmodels/__init__.py
+++ src/viewmodels/__init__.py
@@ -1,1 +1,8 @@
 """ViewModels package - Data transformation layer between controllers and views."""
+
+from viewmodels.email_viewmodel import EmailSuggestionViewModel, EmailDetailViewModel
+
+__all__ = [
+    'EmailSuggestionViewModel',
+    'EmailDetailViewModel',
+]
