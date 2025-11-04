507a5fdc79d03b62359999ecf18581ebf646b2cb
Amelia Payne <ameliapayne@microsoft.com>
2025-11-03 17:20:57 -0800

    Delete the python backend

--- backend/services/outlook/adapters/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Outlook Email Adapter module.
-
-This package provides the OutlookEmailAdapter that wraps OutlookManager
-to implement the EmailProvider interface.
-"""
-
-from backend.services.outlook.adapters.outlook_email_adapter import OutlookEmailAdapter
-
-__all__ = ['OutlookEmailAdapter']
